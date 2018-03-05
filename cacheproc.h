a25275caac7cfb16a3e3b2a5d946a5ee71c3afce
Alexey Tourbin <alexey.tourbin@gmail.com>
2018-03-05 17:43:42 +0300

    rpmfile2.c: removed cacheproc and frenc, prints to stdout directly
    The idea was to serialize records, per package, so that they could
    be cached and then printed from the cache.  This was intended as
    a preliminary exercise to sharpen the techniques that could later
    be used to deal with elf symbols.
    
    Anyway, the <frnec.h> library is nowhere near being ready,
    and <rpmcache.h> is even less so.  This is not because I was being
    lazy, but rather because it's not at all clear what could be the
    best way to do certain things.  Take the cache, for example.
    If a cache is backed by a hard drive, and the entries are accessed
    in random order, than the user should be aware that, unless he's lucky,
    he or she can only retrieve about 100 cache entries per second.
    So it can take 200 seconds to process 20,000 packages.  Who needs
    such a cache?
    
    So instead, this change introduces some optimizations in filename
    handling: filenames are split into dirnames+basenames, and successive
    dirnames are reused.

--- cacheproc.h
+++
@@ -1,4 +0,0 @@
-void cacheproc(const char *cache, const char *dir, char **rpms, size_t nrpms,
-	void (*gen)(const char *rpm, void **data, size_t *size, void *arg),
-	void (*put)(const char *rpm, void *data, size_t size, void *arg),
-	bool order, void *arg);
--- rpmfile2.c
+++ rpmfile2.c
@@ -1,16 +1,13 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <assert.h>
-#include <limits.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/mman.h>
 #include <magic.h>
-#include <frenc.h>
 #include "rpmcpio.h"
 #include "cpioproc.h"
-#include "cacheproc.h"
 #include "xwrite.h"
 #include "errexit.h"
 
@@ -116,125 +113,231 @@ void tmpbuf_fill(struct tmpbuf *t, struct rpmcpio *cpio, const struct cpioent *e
 #define NTMP (CPIOPROC_NQ+2)
 struct tmpbuf g_tmpbuf[NTMP];
 
-// file+type entry
+// The global libmagic handle, only used in the cpioproc thread.
+magic_t g_magic;
+
+// A signle classified cpio entry, file+type.
 struct ft {
-    const char *name;
-    const char *type;
+    char *bn;
+    char *dn;
+    char *type;
+    unsigned blen;
+    unsigned dlen;
+    unsigned tlen;
     unsigned short mode;
-    unsigned typeno;
-    unsigned modeno;
+    bool talloc;
+    char pad;
 };
 
 // passed around when generating the record for a package
-struct arg {
-    magic_t magic;
+struct ctx {
+    const char *rpmbname;
+    // total number of entries peeked/processed
+    unsigned nent;
     // tmpbuf sequence/counter
-    unsigned ntmp;
+    unsigned njob;
     // ft[i] can be accessed without locking, provided that
-    // the thread knows its beforehand (i.e. uses ent->no)
-    struct ft ft[];
+    // the thread knows its index beforehand (i.e. uses ent->no)
+    struct ft *ft;
 };
 
 // cpioproc callback
-static void *peek(struct rpmcpio *cpio, const struct cpioent *ent, void *a)
+static void *peek(struct rpmcpio *cpio, const struct cpioent *ent, void *arg)
 {
-    struct arg *arg = a;
-    struct ft *f = &arg->ft[ent->no];
-    f->name = memcpy(xmalloc(ent->fnamelen + 1), ent->fname, ent->fnamelen + 1);
+    struct ctx *ctx = arg;
+    assert(ctx->nent == ent->no);
+    ctx->nent++;
+    struct ft *f = &ctx->ft[ent->no];
     f->mode = ent->mode;
+    // Basename is always allocated.  Dirname is either reused from the
+    // previous entry or placed in the same malloc chunk after basename.
+    char *dn = NULL, *bn = NULL;
+    size_t dlen = 0, blen = 0;
+    if (*ent->fname != '/') {
+	blen = ent->fnamelen;
+	bn = memcpy(xmalloc(blen + 1), ent->fname, blen + 1);
+    }
+    else {
+	bn = strrchr(ent->fname, '/') + 1;
+	dlen = bn - ent->fname;
+	blen = ent->fnamelen - dlen;
+	if (ent->no && f[-1].dlen == dlen
+		    && memcmp(f[-1].dn, ent->fname, dlen) == 0) {
+	    dn = f[-1].dn;
+	    bn = memcpy(xmalloc(blen + 1), bn, blen + 1);
+	}
+	else {
+	    bn = memcpy(xmalloc(blen + dlen + 2), bn, blen + 1);
+	    dn = memcpy(bn + blen + 1, ent->fname, dlen);
+	    dn[dlen] = '\0';
+	}
+    }
+    f->bn = bn, f->blen = blen;
+    f->dn = dn, f->dlen = dlen;
     // file(1) only really looks at size > 1.
     if (S_ISREG(ent->mode) && ent->size > 1) {
 	// Offloading tmpbuf as a job for cpioproc.
-	struct tmpbuf *t = &g_tmpbuf[arg->ntmp++ % NTMP];
+	struct tmpbuf *t = &g_tmpbuf[ctx->njob++ % NTMP];
 	if (!t->buf)
 	    tmpbuf_init(t);
 	tmpbuf_fill(t, cpio, ent);
 	return t;
     }
-    // Regular files and hardlinks always have types (stored in
-    // rpmfile records); other kinds of files never have types.
+    // For regulare files, a distinction still remains between empty and
+    // non-empty files (the file size is no longer available at print time).
     if (S_ISREG(ent->mode)) {
+#define S0 "empty"
+#define S1 "very short file (no magic)"
 	if (ent->size == 0)
-	    f->type = "empty";
+	    f->type = S0, f->tlen = sizeof S0 - 1;
 	else
-	    f->type = "very short file (no magic)";
+	    f->type = S1, f->tlen = sizeof S1 - 1;
+	f->talloc = false;
+#undef S0
+#undef S1
     }
+    // For symbolic links, will need to print the target.
     else if (S_ISLNK(ent->mode)) {
-	char *buf = alloca(ent->size + 1);
+	char *buf = xmalloc(ent->size + 1);
 	rpmcpio_readlink(cpio, buf, ent->size + 1);
-	f->type = buf;
+	f->type = buf, f->tlen = ent->size, f->talloc = true;
     }
-    else {
-	f->type = NULL;
-	return NULL;
-    }
-    size_t len = strlen(f->type);
-    f->type = memcpy(xmalloc(len + 1), f->type, len + 1);
-    return NULL;
+    else // File type deducible from ent->mode at print time.
+	f->type = NULL, f->tlen = 0, f->talloc = false;
+    return NULL; // no job
 }
 
 // cpioproc callback, handles offloaded jobs
-static void proc(void *tmpbuf, void *a)
+static void proc(void *tmpbuf, void *arg)
 {
     struct tmpbuf *t = tmpbuf;
-    struct arg *arg = a;
+    struct ctx *ctx = arg;
     const char *type;
     if (t->elf) {
 	// magic_descriptor closes the fd.
 	int fd = dup(t->fd);
 	if (fd < 0)
 	    die("%s: %m", "dup");
-	type = magic_descriptor(arg->magic, fd);
+	type = magic_descriptor(g_magic, fd);
     }
     else {
 	size_t size = t->size < MBUFSIZ ? t->size : MBUFSIZ;
-	type = magic_buffer(arg->magic, t->buf, size);
+	type = magic_buffer(g_magic, t->buf, size);
     }
-    struct ft *f = &arg->ft[t->no];
-    if (!type)
-	die("%s: magic failure", f->name);
+    struct ft *f = &ctx->ft[t->no];
+    if (!(type && *type))
+	die("%s: %s%s: magic failure",
+	    ctx->rpmbname, f->dn ? f->dn : "", f->bn);
+#define MAXTLEN 4095
     size_t tlen = strlen(type);
-    f->type = memcpy(xmalloc(tlen + 1), type, tlen + 1);
+    if (tlen > MAXTLEN)
+	die("%s: %s%s: magic type too long",
+	    ctx->rpmbname, f->dn ? f->dn : "", f->bn);
+    f->tlen = tlen;
+    // Try to reuse the string from the previous entry.
+    if (t->no && f[-1].tlen == f->tlen
+	      && memcmp(f[-1].type, type, tlen) == 0)
+	f->type = f[-1].type, f->talloc = false;
+    else
+	f->type = memcpy(xmalloc(tlen + 1), type, tlen + 1),
+	    f->talloc = true;
 }
 
-static int namecmp(const void *a1, const void *a2)
+// Compare two strings whose lengths are known.
+static inline int strlencmp(const char *s1, size_t len1, const char *s2, size_t len2)
 {
-    const struct ft *ft1 = a1;
-    const struct ft *ft2 = a2;
-    return strcmp(ft1->name, ft2->name);
+    if (len1 < len2) {
+	int cmp = memcmp(s1, s2, len1);
+	// If cmp == 0, still s1 < s2, because s1 is shorter.
+	return cmp - (cmp == 0);
+    }
+    int cmp = memcmp(s1, s2, len2);
+    // If cmp == 0, then s1 >= s2, because s1 may be longer.
+    return cmp + ((cmp == 0) & (len1 > len2));
 }
 
-static int typecmp(const void *a1, const void *a2)
+// Compare ft[] filenames for the sort routine.
+static inline int fnamecmp(const char *rpmbname, struct ft *f1, struct ft *f2)
 {
-    const struct ft *ft1 = a1;
-    const struct ft *ft2 = a2;
-    // NULLs go in the end
-    if (ft1->type == NULL)
-	return 1 - (ft2->type == NULL);
-    else if (ft2->type == NULL)
-	return -1;
-    return strcmp(ft1->type, ft2->type);
+    assert(!f1->dn ^ !!f2->dn); // both dirnames are NULL, or both non-NULL
+    if (f1->dn != f2->dn) {
+	int cmp = strlencmp(f1->dn, f1->dlen, f2->dn, f2->dlen);
+	if (cmp)
+	    return cmp;
+    }
+    int cmp = strlencmp(f1->bn, f1->blen, f2->bn, f2->blen);
+    if (cmp == 0)
+	die("%s: %s%s: filename dup", rpmbname, f1->dn ? f1->dn : "", f1->bn);
+    return cmp;
 }
 
-// rpmfile record format
-//
-//	tsize[4] fsize[4] msize[1]
-//	tenc[tsize] fenc[fsize] pad[0-1]
-//	mtab : mode[2]... ix[1]... | mode[2]...
-//	ttab : ix[1]... | ix[2]...
-//
-// Little-endian tsize and fsize are the sizes of file(1) types (tenc)
-// and filenames (fenc), both encoded with frenc.  When msize byte != 0,
-// a compactified modes[] table is used: first goes the table of commonly
-// used msize modes, and then single-byte indexes into the table (per file).
-// Otherwise (msize=0), two-byte modes are per file.
-//
-// Then go type indexes into tenc, single byte if fsize <= 256, otherwise
-// two-byte little-endian integers.  Indexes are only provided for regular
-// files and symlinks (otherwise there's no magic type and no additional
-// information).
+// Sort ft[] by filename.  Insertion sort should suffice, because filenames
+// are "almost sorted" (only hard links are grouped at the end of payload).
+void sort(const char *rpmbname, struct ft ft[], size_t n)
+{
+    // For each item starting with the second...
+    for (size_t i = 1; i < n; i++) {
+	if (fnamecmp(rpmbname, &ft[i-1], &ft[i]) < 0)
+	    continue;
+	// move it down the array so that the first part is sorted.
+	size_t j = i;
+	struct ft save = ft[j];
+	do
+	    ft[j] = ft[j-1], j--;
+	while (j && fnamecmp(rpmbname, &ft[j-1], &save) > 0);
+	ft[j] = save;
+    }
+}
 
-void gen(const char *rpmfname, void **data, size_t *size, void *magic)
+// Print a buffer to stdout.
+static inline void printbuf(const void *buf, size_t size)
+{
+    if (fwrite_unlocked(buf, 1, size, stdout) != size)
+	die("%s: %m", "fwrite");
+}
+
+// Format a 6-digit octal number.
+static inline void oct6(unsigned u, char o[6])
+{
+    o[0] = '0' + ((u >> 15) & 7);
+    o[1] = '0' + ((u >> 12) & 7);
+    o[2] = '0' + ((u >>  9) & 7);
+    o[3] = '0' + ((u >>  6) & 7);
+    o[4] = '0' + ((u >>  3) & 7);
+    o[5] = '0' + ((u >>  0) & 7);
+}
+
+// Print a single entry.
+void print1(struct ft *f)
+{
+    if (f->dn)
+	printbuf(f->dn, f->dlen);
+    printbuf(f->bn, f->blen);
+    // Octal mode - 5 or 6 characters, surrounded by '\t'.
+    char obuf[8];
+    oct6(f->mode, obuf + 1);
+    // Skip the leading zero, if there is one.
+    char *o = obuf + (obuf[1] == '0');
+    size_t olen = 8 - (obuf[1] == '0');
+    *o = obuf[7] = '\t';
+    printbuf(o, olen);
+    // deal with type
+#define SLNK "symbolic link to "
+#define SDIR "directory"
+#define SOTH "other"
+    if (S_ISLNK(f->mode))
+	printbuf(SLNK, sizeof SLNK - 1);
+    if (f->type)
+	printbuf(f->type, f->tlen);
+    else if (S_ISDIR(f->mode))
+	printbuf(SDIR, sizeof SDIR - 1);
+    else
+	printbuf(SOTH, sizeof SOTH - 1);
+    if (putchar_unlocked('\n') != '\n')
+	die("%s: %m", "putchar");
+}
+
+void rpmfile(const char *rpmfname)
 {
     unsigned nent;
     struct rpmcpio *cpio = rpmcpio_open(AT_FDCWD, rpmfname, &nent, false);
@@ -242,205 +345,41 @@ void gen(const char *rpmfname, void **data, size_t *size, void *magic)
 	rpmcpio_close(cpio);
 	return;
     }
-    struct arg *arg = xmalloc(sizeof(*arg) + (nent + 1) * sizeof *arg->ft);
-    struct ft *ft = arg->ft;
-    arg->magic = magic;
-    arg->ntmp = 0;
-    memset(ft, 0, (nent + 1) * sizeof *ft);
-    cpioproc(cpio, peek, proc, arg);
+    const char *rpmbname = strrchr(rpmfname, '/');
+    rpmbname = rpmbname ? rpmbname + 1 : rpmfname;
+    struct ft *ft = reallocarray(NULL, nent, sizeof *ft);
+    if (!ft)
+	die("%s: cannot allocate %u file+type entries", rpmbname, nent);
+    struct ctx ctx = { rpmbname, 0, 0, ft };
+    cpioproc(cpio, peek, proc, &ctx);
     rpmcpio_close(cpio);
-    // actual number of entries can be smaller
-    for (struct ft *p = ft; ; p++)
-	if (p->name == NULL) {
-	    nent = p - ft;
-	    break;
-	}
-    if (nent == 0) {
-	free(arg);
-	return;
+    nent = ctx.nent;
+    // Sort by filename.
+    sort(rpmbname, ft, nent);
+    // Print the entries.
+    for (unsigned i = 0; i < nent; i++)
+	print1(&ft[i]);
+    // Free data.
+    for (unsigned i = 0; i < nent; i++) {
+	struct ft *f = &ft[i];
+	free(f->bn);
+	if (f->talloc)
+	    free(f->type);
     }
-    // build types blob
-    qsort(ft, nent, sizeof *ft, typecmp);
-    int ntypes = 0;
-    void *tblob = NULL;
-    size_t tblobsize = 0;
-    if (ft[0].type) {
-	// merge the same types
-	const char **types = xmalloc(nent * sizeof(*types));
-	ntypes = 1;
-	types[0] = ft[0].type;
-	ft[0].typeno = 0;
-	for (int i = 1; i < nent; i++) {
-	    if (ft[i].type == NULL)
-		break;
-	    if (strcmp(ft[i-1].type, ft[i].type) == 0)
-		ft[i].typeno = ft[i-1].typeno;
-	    else {
-		types[ntypes] = ft[i].type;
-		ft[i].typeno = ntypes++;
-	    }
-	}
-	if (ntypes > 65536)
-	    die("%s: too many file(1) types", xbasename(rpmfname));
-	tblobsize = frenc((char **) types, ntypes, &tblob);
-	if (tblobsize >= FRENC_ERROR)
-	    die("frenc failed");
-	free(types);
-    }
-    // build files blob
-    qsort(ft, nent, sizeof *ft, namecmp);
-    const char **names = xmalloc(nent * sizeof(*names));
-    for (int i = 0; i < nent; i++)
-	names[i] = ft[i].name;
-    void *fblob;
-    const size_t fblobsize = frenc((char **) names, nent, &fblob);
-    if (fblobsize >= FRENC_ERROR)
-	die("frenc failed");
-    free(names);
-    // can free file+type strings
-    for (int i = 0; i < nent; i++)
-	free((char *) ft[i].name);
-    // gen the record
-    size_t blobsize = 9 + tblobsize + fblobsize +
-		     (9 + tblobsize + fblobsize) % 2 +
-		      nent * sizeof(short) +
-		      nent * (1 + (ntypes > 256));
-    char *blob = xmalloc(blobsize);
-    unsigned u = tblobsize; memcpy(blob + 0, &u, 4);
-    unsigned v = fblobsize; memcpy(blob + 4, &v, 4);
-    blob[8] = 0; // no mode maps for now
-    if (tblob) {
-	memcpy(blob + 9, tblob, tblobsize);
-	free(tblob);
-    }
-    memcpy(blob + 9 + tblobsize, fblob, fblobsize);
-    free(fblob);
-    char *p = blob +
-	 9 + tblobsize + fblobsize;
-    if ((9 + tblobsize + fblobsize) % 2)
-	*p++ = '\0';
-    // put modes
-    unsigned short *modes = (void *) p;
-    for (int i = 0; i < nent; i++)
-	*modes++ = ft[i].mode;
-    // put type indexes
-    if (ntypes > 256) {
-	unsigned short *types = modes;
-	for (int i = 0; i < nent; i++)
-	    if (S_ISREG(ft[i].mode) || S_ISLNK(ft[i].mode))
-		*types++ = ft[i].typeno;
-	assert(blob + blobsize >= (char *) types);
-	blobsize = (char *) types - blob;
-    }
-    else {
-	unsigned char *types = (void *) modes;
-	for (int i = 0; i < nent; i++)
-	    if (S_ISREG(ft[i].mode) || S_ISLNK(ft[i].mode))
-		*types++ = ft[i].typeno;
-	assert(blob + blobsize >= (char *) types);
-	blobsize = (char *) types - blob;
-    }
-    free(arg);
-    *data = blob;
-    *size = blobsize;
-}
-
-static inline char *oct(unsigned u, char *end)
-{
-    do
-	*--end = '0' + (u & 7);
-    while (u >>= 3);
-    return end;
-}
-
-#define FWRITE(buf, size)		\
-    do {				\
-	if (fwrite_unlocked(buf, size, 1, stdout) != 1) \
-	    die("fwrite failed");	\
-    } while (0)
-
-void put(const char *rpm, void *data0, size_t size, void *arg)
-{
-    (void) arg;
-    if (size == 0)
-	return;
-    rpm = xbasename(rpm);
-    if (size < 9)
-	die("%s: truncated record", rpm);
-    char *blob = data0;
-    unsigned tblobsize; memcpy(&tblobsize, blob + 0, 4);
-    unsigned fblobsize; memcpy(&fblobsize, blob + 4, 4);
-    // decode types
-    char **types = NULL;
-    size_t ntypes = 0;
-    unsigned *tlens = NULL;
-    if (tblobsize) {
-	ntypes = frdecl(blob + 9, tblobsize, &types, &tlens);
-	if (ntypes >= FRENC_ERROR)
-	    die("%s: frdec failed", rpm);
-	// terminate the strings with '\n'
-	for (size_t i = 0; i < ntypes; i++)
-	    types[i][tlens[i]] = '\n';
-    }
-    // decode filenames
-    char **files;
-    unsigned *flens;
-    size_t nfiles = frdecl(blob + 9 + tblobsize, fblobsize, &files, &flens);
-    if (nfiles >= FRENC_ERROR)
-	die("%s: frdec failed", rpm);
-    char *p = blob +
-	 9 + tblobsize + fblobsize;
-    if ((9 + tblobsize + fblobsize) % 2)
-	if (*p++ != '\0')
-	    die("%s: invalid data (pad)", rpm);
-    unsigned short *modes = (void *) p;
-    union { unsigned char *t8; unsigned short *t16; } u = { (void *) (modes + nfiles) };
-    for (size_t i = 0; i < nfiles; i++) {
-	FWRITE(files[i], flens[i]);
-	unsigned mode = modes[i];
-	{
-	    char buf[8];
-	    buf[7] = '\t';
-	    char *o = oct(mode, buf + 7);
-	    *--o = '\t';
-	    FWRITE(o, buf + 8 - o);
-	}
-	if (S_ISREG(mode) || S_ISLNK(mode)) {
-	    if ((char *) u.t8 >= blob + size)
-		die("%s: invalid data (type index truncated)", rpm);
-	    size_t j;
-	    if (ntypes > 256)
-		j = *u.t16++;
-	    else
-		j = *u.t8++;
-	    if (j >= ntypes)
-		die("%s: invalid data (type index)", rpm);
-	    if (S_ISLNK(mode)) {
-		const char S[] = "symbolic link to ";
-		FWRITE(S, sizeof S - 1);
-	    }
-	    FWRITE(types[j], tlens[j] + 1);
-	}
-#define PRINTLN(s) FWRITE(s "\n", sizeof(s))
-	else if (S_ISDIR(mode))
-	    PRINTLN("directory");
-	else
-	    PRINTLN("other");
-    }
-    free(types);
-    free(files);
+    free(ft);
 }
 
 int main(int argc, char **argv)
 {
     if (argc < 2)
 	die("usage: rpmfile file.rpm...");
-    magic_t magic = magic_open(0);
-    if (!magic)
-	die("cannot open magic");
-    if (magic_load(magic, NULL) < 0)
-	die("cannot load magic");
-    cacheproc("rpmfile", NULL, argv + 1, argc - 1, gen, put, 1, magic);
-    magic_close(magic);
+    g_magic = magic_open(0);
+    if (!g_magic || magic_load(g_magic, NULL) < 0)
+	die("cannot load magic db");
+    for (int i = 1; i < argc; i++)
+	rpmfile(argv[i]);
+    if (fflush(stdout) != 0)
+	die("%s: %m", "fflush");
+    magic_close(g_magic);
     return 0;
 }
